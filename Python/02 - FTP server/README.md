7f87093b2e22e7f430a5c1defbcdf1cd415529f7
randilfernando <randil.fernando@outlook.com>
2018-12-04 07:37:46 +0530

    added comments and refactored code

--- Python/02 - FTP server/README.md
+++ Python/02 - FTP server/README.md
@@ -16,13 +16,25 @@ Run FTP client with `python3 client.py`
 ## Client commands available
 ### con [ip] [port]
 connect with ftp server
+```
+con 127.0.0.1 1026
+```
 ### dis
 disconnect from ftp server
-### cd [directory]
-go to directory inside ftp server
-### ls
-view files inside ftp server directory
+```
+dis
+```
+
 ### up [local_file_path]
 upload file from local to current directory in ftp server
+```
+up /home/user/Documents/file.txt
+```
 ### down [file_name] [local_directory_path]
 download file from current directory in ftp server to local directory
+```
+up file.txt /home/user/Documents
+```
+
+### cd, ls, pwd, clear, cls
+same syntax as in system terminal
--- Python/02 - FTP server/client.py
+++ Python/02 - FTP server/client.py
@@ -11,14 +11,30 @@ connected = False
 
 
 def complete(text, state):
+    """
+    generate path from input string and auto complete
+    :param text: text to complete
+    :param state: input state
+    :return: completed text
+    """
     return (glob.glob(text + '*') + [None])[state]
 
 
 def get_prompt(s):
+    """
+    generate formatted message for the end user
+    :param s: message
+    :return: formatted message
+    """
     return "{0}@anonymous:{1}$ {2}".format(address, pwd, s)
 
 
 def connect(arguments):
+    """
+    connect with the FTP server
+    :param arguments: [ip, port]
+    :return: none
+    """
     global address, connected
 
     if connected:
@@ -28,6 +44,7 @@ def connect(arguments):
     port = int(arguments[1])
 
     try:
+        # connect with the FTP server
         ftp.connect(ip, port)
         ftp.login()
         address = ip
@@ -40,11 +57,16 @@ def connect(arguments):
 
 
 def disconnect():
+    """
+    disconnect with the FTP server
+    :return: none
+    """
     global pwd, address, connected
 
     if not connected:
         return
 
+    # disconnect with the FTP server
     ftp.quit()
 
     pwd = ""
@@ -54,28 +76,45 @@ def disconnect():
 
 
 def go_to_dir(arguments):
+    """
+    change current working directory in the FTP server
+    :param arguments: [directory]
+    :return: none
+    """
     global pwd
 
     if not connected:
         print(get_prompt("please connect first"))
         return
 
+    directory = arguments[0]
+
     try:
-        ftp.cwd(arguments[0])
+        # change current working directory in the FTP server
+        ftp.cwd(directory)
         pwd = ftp.pwd()
     except all_errors:
         print(get_prompt("invalid directory"))
 
 
 def list_dir():
+    """
+    list directories and files inside current working directory
+    :return: none
+    """
     if not connected:
         print(get_prompt("please connect first"))
         return
 
+    # list directories and files inside current working directory
     ftp.retrlines('LIST')
 
 
 def view_dir():
+    """
+    view current working directory
+    :return: none
+    """
     if not connected:
         print(get_prompt("please connect first"))
         return
@@ -84,47 +123,86 @@ def view_dir():
 
 
 def clear_console(command):
-    def clear(): os.system(command)
+    """
+    clear console window
+    :param command: clear or cls
+    :return: none
+    """
 
+    # get system command and execute
+    def clear(): os.system(command)
     clear()
 
 
 def upload_file(arguments):
+    """
+    upload a file into current working directory of FTP server from source location
+    :param arguments: [file path]
+    :return: none
+    """
     if not connected:
         print(get_prompt("please connect first"))
         return
 
     source = arguments[0]
     filename = os.path.basename(source)
-    ftp.storbinary("STOR " + filename, open(source, 'rb'))
 
-    print(get_prompt("uploaded file {0}".format(source)))
+    # upload a file into the FTP server
+    try:
+        with open(source, 'rb') as f:
+            ftp.storbinary("STOR " + filename, f)
+            print("uploaded file {0}".format(source))
+    except IOError:
+        print("error when uploading file {0}".format(source))
+    except all_errors:
+        print("error when uploading file {0}".format(source))
 
 
 def download_file(arguments):
+    """
+    download a file from current working directory of FTP server to destination location
+    :param arguments: [file name, destination path]
+    :return: none
+    """
     if not connected:
         print(get_prompt("please connect first"))
         return
 
     filename = arguments[0]
     destination = os.path.join(arguments[1], filename)
-    local_file = open(destination, 'wb')
-    ftp.retrbinary("RETR " + filename, local_file.write, 1024)
-    local_file.close()
 
-    print(get_prompt("downloaded file {0}".format(destination)))
+    try:
+        with open(destination, 'wb') as f:
+            ftp.retrbinary("RETR " + filename, f.write, 1024)
+            print("downloaded file {0}".format(destination))
+    except IOError:
+        print("error when downloading file {0}".format(filename))
+    except all_errors:
+        print("error when downloading file {0}".format(filename))
 
 
 def split_arguments(s):
+    """
+    split command line input and extract arguments
+    :param s: command line input
+    :return: (command, arguments)
+    """
+
+    # split with "'"
     parts_quote = s.split("'")
     parts_final = []
 
     for i in range(len(parts_quote)):
         if i == "":
+            # skip empty parts
             continue
         elif i % 2 == 1:
+            # parts surrounded by quotation marks (eg: "word documents.doc")
+            # do not need further splittings
             parts_final.append(parts_quote[i])
         else:
+            # other parts do need further splittings
+            # split and add non empty parts
             parts_final = parts_final + [x for x in parts_quote[i].split(" ") if not x == ""]
 
     command = parts_final[0]
@@ -137,41 +215,60 @@ def split_arguments(s):
 
 
 def parse_command(s):
+    """
+    map command with function to execute
+    :param s: user input
+    :return: none
+    """
     if s == "":
         return
 
     (command, arguments) = split_arguments(s)
 
     if command == "cls" or command == "clear":
+        # example command (mac or linux): clear
+        # example command (windows): cls
         clear_console(command)
     elif command == "pwd":
+        # example command: pwd
         view_dir()
     elif command == "cd":
+        # example command: cd /home/user/Documents
         go_to_dir(arguments)
     elif command == "ls":
+        # example command: ls
         list_dir()
     elif command == "con":
+        # example command: con 127.0.0.1 1026
         connect(arguments)
     elif command == "dis":
+        # example command: dis
         disconnect()
     elif command == "up":
+        # example command: up /home/user/Documents/file.txt
+        # will upload into the current working directory of FTP server
         upload_file(arguments)
     elif command == "down":
+        # example command: down file.txt /home/user/Documents
+        # download file inside current working directory of FTP server
         download_file(arguments)
     else:
         print(get_prompt("invalid command"))
 
 
 def main():
+    # enable auto completion for paths (optional)
     readline.set_completer_delims(' \t\n;')
     readline.parse_and_bind("tab: complete")
     readline.set_completer(complete)
 
+    # start application and get user input
     s = input(get_prompt(""))
     while s != 'exit':
         parse_command(s)
         s = input(get_prompt(""))
 
+    # clean up
     disconnect()
 
 
